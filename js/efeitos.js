089ee2c33019f0a6662d97ae2240fe6873b112a8
carloshenriqueok <ifcarloshenrique@gmail.com>
2025-01-09 14:47:58 -0300

    colocando o btn ler mais pa funcionar

--- js/efeitos.js
+++ js/efeitos.js
@@ -1,4 +1,15 @@
+//SCROLL DO HEADER
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header');
     header.classList.toggle('rolagem', window.scrollY > 0);
+});
+
+//BTN LER MAIS E MENOS
+
+const button = document.getElementById('ler-mais');
+button.addEventListener('click', function(){
+    const card = document.querySelector('.card');
+    card.classList.toggle('ativado');
+
+    //if (car)
 });
