81e19c6044888603868531589dea206faaa4a416
carloshenriqueok <ifcarloshenrique@gmail.com>
2025-01-21 15:42:22 -0300

    inicio da dinamica com js

--- js/efeitos.js
+++ js/efeitos.js
@@ -37,3 +37,44 @@ navButton.addEventListener('click', function(){
      document.querySelector('.icon').src = "images/close_white_36dp.svg";
     }
 })
+
+
+//TEMPO DE TELA RECOMENDADO (PAG USO CONSCIENTE)
+
+let btnEnviar = document.getElementById('btn-enviar');
+
+btnEnviar.addEventListener('click', function(){
+    let idade = parseInt(document.getElementById('idade').value);
+    let horas = parseFloat(document.getElementById('horas').value.replace(":" , "."));
+    let saida = document.getElementById('saida');
+
+
+    if(idade <= 0 || idade > 99){
+        alert("DADOS INVÁLIDOS \nSó são aceitas idades maiores que 0 e menores que 99");
+    }
+
+    if(horas < 0 || horas > 24){
+        alert("DADOS INVÁLIDOS \nSó são aceitas horas positivas e menor ou igual a 24 horas")
+    }
+
+    if(idade >=1 && idade <= 2){
+        if(horas > 0){
+            saida.innerHTML = `
+        <div class="resultado">
+          <img src="images/Disappointed.png" alt="carinha-triste">
+          <h3 class="vermelho">Acima do tempo recomendado</h3>
+        </div>
+        <p class="media">Sua média diaria: ${horas.toFixed(2).replace("." , ":")} horas</p>
+        <p class="recomendado">Média diária recomendada: 0 horas</p>`;
+        }
+        else{
+            saida.innerHTML = ` 
+        <div class="resultado">
+          <img src="images/Smiling.png" alt="carinha-triste">
+          <h3 class="verde">Dentro do tempo recomendado</h3>
+        </div>
+        <p class="media">Sua média diaria: ${horas.toFixed(2).replace("." , ":")} horas</p>
+        <p class="recomendado">Média diária recomendada: 0 horas</p>`
+        }
+    }
+});
